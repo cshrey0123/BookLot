52b74e2c8c82553162061611465e73b6c402b8c5
Cshrey123 <114456227+Cshrey123@users.noreply.github.com>
2024-07-13 09:30:21 +0530

    .

--- src/pages/signIn/SignIn.styled.js
+++ src/pages/signIn/SignIn.styled.js
@@ -4,7 +4,7 @@ import img from './bg.png'
 export const StyledSignIn = styled.div`
   height: 100vh;
   width: 100%;
-  /* background-image: url(${img}); */
+  background-image: url(${img}); 
   background-image: url('');
   background-size: cover;
   background-repeat: no-repeat;
