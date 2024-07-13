6b871391887954b0792548c5c326c2b2f9964dac
Cshrey123 <114456227+Cshrey123@users.noreply.github.com>
2024-07-13 16:30:25 +0530

    .

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrQOqy6Fy1i3H5GA0sowqqv1z0GLlbqVg",
-  authDomain: "book-77783.firebaseapp.com",
-  projectId: "book-77783",
-  storageBucket: "book-77783.appspot.com",
-  messagingSenderId: "580653667800",
-  appId: "1:580653667800:web:e802bb6ae3c3d17bf6cead",
-  measurementId: "G-04W53KW2JP"
+  apiKey: "AIzaSyBM7--UHmPxp_k3APPrljU1XB1EtyExUqc",
+  authDomain: "lastapi-d6b5e.firebaseapp.com",
+  projectId: "lastapi-d6b5e",
+  storageBucket: "lastapi-d6b5e.appspot.com",
+  messagingSenderId: "518842752879",
+  appId: "1:518842752879:web:952616e026dabe62dc129a",
+  measurementId: "G-3624XENFS5"
 };
 
 // Initialize Firebase
