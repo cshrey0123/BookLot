e7d443f0cb127a6d61add9f8f6e1782785ef3aec
Cshrey123 <114456227+Cshrey123@users.noreply.github.com>
2024-07-13 16:10:23 +0530

    .

--- src/pages/signIn/SignIn.styled.js
+++ src/pages/signIn/SignIn.styled.js
@@ -5,7 +5,7 @@ export const StyledSignIn = styled.div`
   height: 100vh;
   width: 100%;
   background-image: url(${img}); 
-  background-image: url('');
+  background-image: url('https://img.freepik.com/free-photo/top-view-hardback-books-with-copy-space_23-2148827214.jpg?t=st=1720867124~exp=1720870724~hmac=a4d2a78826be3fdf014802c3090a5221b91336ed5f1e7158dbc266fcfb9a60b5&w=996');
   background-size: cover;
   background-repeat: no-repeat;
   background-position: bottom;
